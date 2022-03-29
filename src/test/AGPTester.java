dea80e552e512d0affb5219b4e56f4a9fe92a102
Like-China <likemelikeyou@126.com>
2022-03-29 17:18:44 +0800

    修改部分变量名

--- src/test/AGPTester.java
+++ src/test/AGPTester.java
@@ -15,15 +15,15 @@ public class AGPTester {
 	
 	public static void main(String[] args) {
 		// 1. 获取该采样率下的所有轨迹点文件名
-		File[] files = new File("D:/data/" + Settings.city_name + Settings.sr).listFiles();
+		File[] files = new File("E:/data/contact_tracer/" + Settings.city_name + Settings.sr).listFiles();
 		// 2. 创建一个Tracer实例对象，处理实时读取到的轨迹位置点
 		AGP_Tracer tracer = new AGP_Tracer(Settings.distance_threshold, Settings.duration_threshold, Settings.city_name);
 		// 3. 初始化一批query 
 		tracer.patientIDs = Util.initPatientIds(Settings.objectNum, Settings.initPatientNum, Settings.isRandom);
-		// 4. 记录一些统计数据
+		// 4. 记录统计数据
 		long AGP_time = 0;
 		int location_num = 0;
-		int current_day = 0;
+		int dayNum = 0;
 		int current_ts = 0;
 		HashMap<Integer, ArrayList<Integer>> AGP_res = new HashMap<Integer, ArrayList<Integer>>();
 		// 4. 开始流形式查询
@@ -32,25 +32,34 @@ public class AGPTester {
 			Stream stream  = new Stream(f.toString());
 			ArrayList<Location> batch = stream.read_batch(); // 流式读取当天一个时刻的位置点
 			while (batch != null && !batch.isEmpty())
-			{  
-				if(batch == null || batch.isEmpty()) break; // 如果当天时间流已经结束，则跳出循环进行下一天的查询，感染时长仍然会累计
-				if (batch.get(0).ts % Settings.sr != 0) continue; // 如果不在采样时间点上，不处理
+			{
+				if (batch.get(0).ts % Settings.sr != 0)
+				{
+					continue; // 如果不在采样时间点上，不处理
+				}
 				location_num += batch.size();
-				System.out.printf("\n%s %s return locations %d\n", batch.get(0).date, batch.get(0).time, batch.size());
+				System.out.printf("\n%s %s return locations %d", batch.get(0).date, batch.get(0).time, batch.size());
 				// AGP查询
 				long startTime = System.currentTimeMillis();
 				ArrayList<Integer> AGP_cases = tracer.trace(batch, current_ts);
-				if(!AGP_cases.isEmpty()) AGP_res.put(current_ts, AGP_cases);
+				// 添加新增病例
+				if(!AGP_cases.isEmpty())
+				{
+					AGP_res.put(current_ts, AGP_cases);
+				}
 				long endTime = System.currentTimeMillis();
-				System.out.println("用时: "+ (endTime-startTime));
 				AGP_time += endTime-startTime;
 				current_ts += 1;
 				batch = stream.read_batch();
 			} // End While Loop
-			current_day += 1;
-			if (current_day == 1) break;
+			dayNum += 1;
+			if (dayNum >= Settings.days) {
+				break;
+			}
 		} // End For Loop
-		// 展示一些结果
+
+
+		// 展示实验结果
 		System.out.printf("共处理%d个位置， %d个时刻 " , location_num, current_ts);
 		System.out.println("用时:  " + AGP_time + " 平均用时:  " + (double)AGP_time/current_ts);
 		HashSet<Integer>  AGP_cases = new HashSet<Integer>();
--- src/trace/Settings.java
+++ src/trace/Settings.java
@@ -4,11 +4,11 @@ public class Settings {
 	
 	public static int days = 12;
 	public static int sr = 50; // 读取的轨迹采样率
-	public static int duration_threshold = 50; // 设定的感染时长
-	public static float distance_threshold = 10f; // 设定的感染距离
+	public static int duration_threshold = 30; // 设定的感染时长
+	public static float distance_threshold = 1f; // 设定的感染距离
 	public static String city_name = "beijing";
 	public static int objectNum = (city_name == "beijing") ? 10000:1000000; // 初始化物体数目
-	public static int initPatientNum = (city_name == "beijing") ? 600:60000;// 初始化病人数目
+	public static int initPatientNum = (city_name == "beijing") ? 500:60000;// 初始化病人数目
 	public static boolean isRandom = false; // 病人的生成是否随机
 	
 	public Settings() {
